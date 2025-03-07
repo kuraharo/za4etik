e4a5fc773521a170f5c1def3241677d5230bb3d8
kuraharo <kuraharo@mail.ru>
2025-03-07 16:09:56 +0300

    Add files via upload

--- #abbr.py
+++ #abbr.py
@@ -1,21 +1,9 @@
 #abbr
 input_string=input()
-input_string=" "+input_string #добавляем пробел, чтобы цикл начинался с него
+matrix_strings_without_space=input_string.split(' ')
 output_string=""
-for index in range(0,len(input_string)-1):
-    index_next=index+1
-    simvol_current=input_string[index]
-    simvol_next=input_string[index_next]
-    nomer_simvola_next=ord(simvol_next)
-    if simvol_current==" " and ((nomer_simvola_next<=ord('z') and nomer_simvola_next>=ord('A')) or (nomer_simvola_next<=ord('я') and nomer_simvola_next>=ord('А'))):
-        if nomer_simvola_next<=ord('z') and nomer_simvola_next>=ord('A'): #слово с англ буквы
-            if(nomer_simvola_next>=ord("a")):
-                nomer_simvola_next-=32
-            simvol_next_big=chr(nomer_simvola_next)
-            output_string+=simvol_next_big
-        else:  #слово с русской буквы
-            if(nomer_simvola_next>=ord("а")):
-                nomer_simvola_next-=32
-            simvol_next_big=chr(nomer_simvola_next)
-            output_string+=simvol_next_big
+for item in matrix_strings_without_space:
+    first_simvol=item[0]
+    if first_simvol.isalpha():
+        output_string=output_string+first_simvol.capitalize()
 print(output_string)
