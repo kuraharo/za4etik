3f7fc0f476ffe1f5b19b80159edfbf6297ae4bb0
kuraharo <kuraharo@mail.ru>
2025-03-07 16:25:13 +0300

    Add files via upload

--- reverse.py
+++ reverse.py
@@ -2,10 +2,8 @@ string_input=input()
 digit=int(input())
 string_output=""
 index=0
-while index+digit<len(string_input)+1:
+delta=2*digit
+for index in range(index,len(string_input),delta):
     string_srez=string_input[index:index+digit]
-    string_output=string_output+string_srez[::-1]+string_input[index+digit:index+2*digit]
-    index+=2*digit
-string_output+=string_input[index:-1]
+    string_output=string_output+string_srez[::-1]+string_input[index+digit:index+delta]
 print(string_output)
-
