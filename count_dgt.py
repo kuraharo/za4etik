da3a2088f135d32d817ad7c1bf750625a88233c4
kuraharo <kuraharo@mail.ru>
2025-03-07 16:17:23 +0300

    Add files via upload

--- count_dgt.py
+++ count_dgt.py
@@ -1,9 +1,14 @@
 digit_input=input()
-dictionary={"0":0,"1":0,"2":0,"3":0,"4":0,"5":0, "6":0,"7":0,"8":0,"9":0}
+dictionary={}
 
 
 for index in range(len(digit_input)):
     digit_current=digit_input[index]
-    dictionary[digit_current]+=1
-for index in range(0,10):
-    print("число", index,"встретилось раз:",dictionary[str(index)],sep=" ")
+    if  digit_current in dictionary:
+        dictionary[digit_current]+=1
+    else:
+        dictionary[digit_current]=1
+
+print(dictionary)
+for digit in dictionary:
+    print("число", digit,"встретилось раз:",dictionary[digit],sep=" ")
